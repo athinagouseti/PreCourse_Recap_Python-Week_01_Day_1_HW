ad5f9d9d5e283e5d3ca8dfacaf4dfb75e7409cbd
Athina Gouseti <athina@Athinas-MacBook-Pro.local>
2021-08-30 18:14:44 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+user = input("What user are you? ")
+
+
+if user == "teacher":
+    print("You may enter!")
+elif user == "other staff":
+    print("Maybe next time")
+else:
+    print("You shall not pass!")
